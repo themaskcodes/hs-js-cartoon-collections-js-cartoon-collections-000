efb4468d8bd70ef0929b40a09894dc85c41aad2f
Chris Combs <chriscombs00@yahoo.com>
2020-02-10 23:22:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,54 @@
+var dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"];
+
 function dwarfRollCall(dwarves) {
+  var rollcall = "";
+  for (var i = 0; i < dwarves.length; i++) {
+    rollcall = rollcall + `${i+1}. ${dwarves[i]} `;
+    
+  }
+  return rollcall;
+  
 }
 
-function summonCaptainPlanet(planeteerCalls){
+
+planeteerCalls = ["earth", "wind", "fire", "water", "heart"];
+
+function summonCaptainPlanet (planeteerCalls) {
+	var newCalls = [];
+	for (var i = 0; i < planeteerCalls.length; i++) {
+		var call = planeteerCalls[i].toUpperCase() + "!";
+		newCalls.push(call);
+	}
+	return newCalls;
 }
+
+
+shortWords = ["wind", "fire"];
+assortedWords = ["earth", "wind", "heart", "fire"];
 
 function longPlaneteerCalls(words) {
+  var result = false
+for(var i = 0; i < words.length; i++) {
+	if(words[i].length > 4) {
+	result = true	}
+}
+return result
 }
 
-function findTheCheese (foods) {
+
+
+function findTheCheese(maybeCheese) {
+  var cheeses = ["cheddar", "gouda", "camembert"]
+  //iterate over maybeCheese check if any of the items inside of maybeCheese exists in cheeses, and if they do, return that cheese. and if not return noCheese.
+  
 }
+
+    
+
+
+
+
+
+
+
+
